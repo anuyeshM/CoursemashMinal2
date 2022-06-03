c38fac15fe68f654e5ebcc65df8b968a6db65d44
Anuyesh Mishra <95938949+anuyeshM@users.noreply.github.com>
2022-06-03 11:47:20 +0530

    Add files via upload

---
+++ build/precache-manifest.a95c7a4c8bf0bbc798a4c13de03a5238.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b55366e815d6b94e988",
+    "url": "/static/css/main.72454a3a.chunk.css"
+  },
+  {
+    "revision": "8b55366e815d6b94e988",
+    "url": "/static/js/main.8b55366e.chunk.js"
+  },
+  {
+    "revision": "21c66070422ab1d72bdb",
+    "url": "/static/js/1.21c66070.chunk.js"
+  },
+  {
+    "revision": "74b82cf082326d32c4b7",
+    "url": "/static/js/2.74b82cf0.chunk.js"
+  },
+  {
+    "revision": "3c9d10709899fa93cba4",
+    "url": "/static/js/runtime~main.3c9d1070.js"
+  },
+  {
+    "revision": "2524428719ff5baa692a1c3631dd6d21",
+    "url": "/static/media/hero-b.25244287.png"
+  },
+  {
+    "revision": "1793da33fb666ee72dd63346d9d0306c",
+    "url": "/static/media/outcomes.1793da33.png"
+  },
+  {
+    "revision": "655e7cb9efa2433892a5e96323e11e08",
+    "url": "/static/media/Image-Next-Step.655e7cb9.png"
+  },
+  {
+    "revision": "35c3460d6d08cc093d79a5c26535d507",
+    "url": "/static/media/contact.35c3460d.png"
+  },
+  {
+    "revision": "74b7ac1584bd9550992b08301a50a04e",
+    "url": "/static/media/certificate (1).74b7ac15.png"
+  },
+  {
+    "revision": "07b7299b8962c6a464c60947f99c16f2",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/BannerHome/BannerHome.js
@@ -0,0 +1,50 @@
+import React from 'react';
+//import banner image
+import BannerImg from '../../Images/hero-b.png';
+import { useHistory } from "react-router-dom";
+import Typewriter from '../../../node_modules/typewriter-effect';
+import ReactGA from '../../../node_modules/react-ga';
+
+
+    
+
+
+const BannerHome = (props) => {
+    
+    const history = useHistory();
+    const navigateTo = () => history.push('/Profile')
+    const ClickHandler = () =>{
+        ReactGA.event({
+            category: 'Button',
+            action: 'Click this button'
+        })
+        
+    }
+    return (
+        <section className='container my-lg-5'>
+            <div className='row'>
+                {/* banner left part text */}
+                <div className="col-md-6 d-flex align-items-center">
+                    <div>
+                        <h1 className='display-3 fw-bold'><Typewriter
+  options={{
+    strings: ['Best Learning Experience is Here'],
+    autoStart: true,
+    loop: true,
+  }}
+/></h1>
+                        <p>Make smart friends, learn from experts, and network like you have never done before.</p>
+                        <button className='btn btn-primary px-4 py-2 me-2' onClick={()=>{navigateTo(); ClickHandler()}}>Join now!</button>
+                        
+                    </div>
+                </div>
+                {/* banner right part images */}
+                <div className="col-md-6 text-end pe-5">
+                    <img className='img-fluid' src={BannerImg} alt="" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BannerHome;
